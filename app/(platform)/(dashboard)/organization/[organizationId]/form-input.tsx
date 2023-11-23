b6939db8682492104bb03a0cb50c04c8f48de572
Hayden Ngo <hoango1607@gmail.com>
2023-11-23 18:20:29 +1030

    feat: add & delete board

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useFormStatus } from "react-dom";
+
+interface FormInputProps {
+  errors?: {
+    title?: string[];
+  };
+}
+
+export const FormInput = ({ errors }: FormInputProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <div>
+      <Input
+        required
+        placeholder="Enter a board title"
+        name="title"
+        id="title"
+        disabled={pending}
+      />
+      {errors?.title ? (
+        <div>
+          {errors.title.map((error: string) => (
+            <p key={error} className="text-rose-500">
+              {error}
+            </p>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+};
