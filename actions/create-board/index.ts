d85ce24c567d9087bbc5254555d48b75d17661ca
Hayden Ngo <hoango1607@gmail.com>
2023-11-26 15:23:01 +1030

    feat: board list

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,22 +1,39 @@
 "use server";
 
-import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
-import { InputType, ReturnType } from "./types";
 import { auth } from "@clerk/nextjs";
-import { CreateBoard } from "./schema";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
 
-const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
+import { InputType, ReturnType } from "./types";
+import { CreateBoard } from "./schema";
 
-  if (!userId) {
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
     return {
       error: "Unauthorized",
     };
   }
 
-  const { title } = data;
+  const { title, image } = data;
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split("|");
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageUserName ||
+    !imageLinkHTML
+  ) {
+    return {
+      error: "Missing fields. Failed to create board.",
+    };
+  }
 
   let board;
 
@@ -24,6 +41,12 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
       },
     });
   } catch (error) {
@@ -32,7 +55,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  revalidatePath(`board/${board.id}`);
+  revalidatePath(`/board/${board.id}`);
   return { data: board };
 };
 
