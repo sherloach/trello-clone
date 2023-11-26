0563cceaba1b827d4f1e9bc4d0e975c41ed0f2c7
Hayden Ngo <hoango1607@gmail.com>
2023-11-26 23:15:43 +1030

    feat: board page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,6 +1,21 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "./_components/org-control";
 
-const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+};
+
+const OrganizationIdLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
   return (
     <>
       <OrgControl />
